f1c6a17659a646eb7b5bc5ecbbf89c4491b112bd
Refffy <gvertyes124@gmail.com>
2021-12-10 20:05:52 +0600

    Box coordinates x, y axises fixed

--- triangles.py
+++ triangles.py
@@ -1,14 +1,5 @@
-from PIL import Image
+from PIL import Image, ImageOps
 from sys import argv
-
-
-def process_tile(tile, y, r, g, b, block_y):
-    for i in range(y+1000):
-        for j in range(i, block_y):
-            tile.putpixel((j, i), (r, g, b))
-            tile.putpixel((i, j), ((r * 3) // 2, (g * 3) //2 , (b * 3) // 2))
-
-    return tile
 
 
 def triangulize(img, block_size):
@@ -16,14 +7,18 @@ def triangulize(img, block_size):
         w, h = img.size
         block_x, block_y = block_size
 
-        for y in range(0, h, block_y):
-            for x in range(0, w, block_x):
-                box = (x, y, x + block_x, y + block_y)
-                tiled_img = img.crop(box)
+        for x in range(0, w, block_x):
+            for y in range(0, h, block_y):
+                box = (y, x, y+block_x, x+block_y)
 
+                tiled_img = img.crop(box)
                 r, g, b = img.getpixel((x, y))
-                processed_tile = process_tile(tiled_img, y, r, g, b, block_y)
-                img.paste(processed_tile, (x, y))
+
+                for i in range(x+block_x):
+                    for j in range(i, block_y):
+                        tiled_img.putpixel((i, j), (r, g, b))
+                        tiled_img.putpixel((j, i), ((r * 3) // 2, (g * 3) //2 , (b * 3) // 2))
+                img.paste(tiled_img, (x, y))
 
         img.show()
 
